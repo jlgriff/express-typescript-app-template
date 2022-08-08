f15a5f5a607c673ebcc18fa71ff3f3b4843f9e8e
John Griffith <john@John-MacBook-Air.local>
2022-08-08 10:05:26 -0400

    Added logging colors to the custom logger.

--- src/utilities/logger.ts
+++ src/utilities/logger.ts
@@ -1,5 +1,25 @@
+const red = '\x1b[31m%s\x1b[0m';
+const yellow = '\x1b[33m%s\x1b[0m';
+const blue = '\x1b[34m%s\x1b[0m';
+const white = '\x1b[37m%s\x1b[0m';
+
+const getLogColor = (level: string): string => {
+  switch (level) {
+    case 'debug':
+      return blue;
+    case 'info':
+      return white;
+    case 'warn':
+      return yellow;
+    case 'error':
+      return red;
+    default:
+      return white;
+  }
+};
+
 const log = (level: 'debug' | 'info' | 'warn' | 'error', message: string) => {
-  console.log(level, '|', message);
+  console.log(getLogColor(level), `${level} | ${message}`);
 };
 
 export default log;
