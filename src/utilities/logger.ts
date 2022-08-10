988552b3aa9e696f14602a4b31a79c18cf7c9b06
John Griffith <john@John-MacBook-Air.local>
2022-08-10 14:22:50 -0400

    Tweaked color of info logs.

--- src/utilities/logger.ts
+++ src/utilities/logger.ts
@@ -1,7 +1,7 @@
+const gray = '\x1b[30m%s\x1b[0m';
 const red = '\x1b[31m%s\x1b[0m';
 const yellow = '\x1b[33m%s\x1b[0m';
 const blue = '\x1b[34m%s\x1b[0m';
-const white = '\x1b[37m%s\x1b[0m';
 
 /**
  * Determines which color corresponds to a log's level
@@ -11,13 +11,13 @@ const getLogger = (level: string): { logger: (...args: any[]) => void, color: st
     case 'debug':
       return { logger: console.debug, color: blue };
     case 'info':
-      return { logger: console.info, color: white };
+      return { logger: console.info, color: gray };
     case 'warn':
       return { logger: console.warn, color: yellow };
     case 'error':
       return { logger: console.error, color: red };
     default:
-      return { logger: console.info, color: white };
+      return { logger: console.info, color: gray };
   }
 };
 
