530c2a382e0f261ad2d95b1f4385ee29de5067b1
jlgriff <jlgriffith@protonmail.com>
2022-11-27 16:39:13 -0500

    Removed custom logger.

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import application from './app.js';
 import { environment, port } from './configs/index.js';
 import { ApplicationConfig } from './interfaces/config.app.js';
-import log from './utilities/logger.js';
 
 const appConfig: ApplicationConfig = { port, environment };
 const app = application();
 const server = app.listen(appConfig.port, () => {
-  log('info', `Application is running on port ${appConfig.port} with the ${appConfig.environment} environment`);
+  console.log(`Application is running on port ${appConfig.port} with the ${appConfig.environment} environment`);
 });
 
 export default server;
